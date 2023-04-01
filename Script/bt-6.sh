3c7cf4f335064c9c79d98e84d908db3077ecfaa3
JP <jpniznhy@hotmail.com>
2023-04-01 12:02:02 +0200

    Add files via upload

--- Script/bt-6.sh
+++ Script/bt-6.sh
@@ -8,7 +8,7 @@
 #SBATCH --qos=debug
 
 # Modificad esta linea con vuestro PATH a los ejecutables
-export NAS_PATH=/home/nct01/nct01175/S2/NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin
+export NAS_PATH=/home/nct01/nct01175/Project/NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin
 
 echo "Using $SLURM_NTASKS processes and $SLURM_CPUS_PER_TASK threads per process"
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
@@ -21,7 +21,7 @@ srun --nodes=6 --ntasks=36 --cpus-per-task=8 $NAS_PATH/bt-mz.D.36
 srun --nodes=6 --ntasks=48 --cpus-per-task=6 $NAS_PATH/bt-mz.D.48
 srun --nodes=6 --ntasks=72 --cpus-per-task=4 $NAS_PATH/bt-mz.D.72
 srun --nodes=6 --ntasks=144 --cpus-per-task=2 $NAS_PATH/bt-mz.D.144
-srun --nodes=6 --ntasks=288 --cpus-per-task=1 $NAS_PATH/bt-mz.D.288
+srun --nodes=6 --ntasks=2w88 --cpus-per-task=1 $NAS_PATH/bt-mz.D.288
 
 
 
